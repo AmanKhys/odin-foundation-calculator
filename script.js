9fbd199e7a6b427f16a5cb4925d5a36b85b0b649
AmanKhys <amankhys2112@gmail.com>
2023-02-10 23:06:28 +0530

    give basic future tasks as comments in code

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function divide(a,b){
     return a/b;
 }
 
+// make buttons for inputs 
+// - js and plain html...as comfortable to write
+// use eventlisteners and read values
+// pass it to functions and update on screen
+// store the result in some variable
+// make sure the presidencies of math are followed
