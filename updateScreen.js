b358d31217b06fbefef13465ac5abdbe81d5748b
AmanKhys <amankhys2112@gmail.com>
2023-02-23 22:19:02 +0530

    workout working logic to get answer from numbers and operators string

--- updateScreen.js
+++ updateScreen.js
@@ -5,26 +5,29 @@
 //wrong logic /// 1*2/2+4-5
 ///I think this function // 1*2 + 2/2 + 2+4 + 4-5 //but that too is not what this function is doing
 let numbers = [1,2,2,4,5];
-let ops2 = ['*','/','+','-'];
+let ops2 = ['/','*','-','-'];
 let len = ops2.length;
-let answer = 0;
 function getAnswer(numbers,ops2){
+    let answer = 0;
     for(let i = 0;i<len;i++){
         let skipper = false;
         ops2.forEach((val,index) => {
             if(val == '/'){
                 if(numbers[index+1] == 0) return '.....math error .....';
-                answer += (numbers[index]/numbers[index+1]); 
+                numbers[index] /=numbers[index+1]; 
+                numbers.splice(index+1,1);
                 ops2.splice(index,1);//=
+                numbers;
                 ops2;
                 skipper = true;
-                answer;
             }
             else if(val == '*'){
-                answer +=(numbers[index]*numbers[index+1]);
-                ops2.splice(index,1);//=
-                skipper = true;
-                answer; //
+                (numbers[index]*=numbers[index+1]);
+                numbers.splice(index+1,1);
+                ops2.splice(index,1);
+                numbers;
+                ops2;//=
+                skipper = true; //
             }
             
         });
@@ -34,21 +37,30 @@ function getAnswer(numbers,ops2){
 
         ops2.forEach((val,index)=>{
             if(val == '+'){
-                answer += numbers[index]+numbers[index+1];
-                answer;
+                numbers[index]+=numbers[index+1];
+                numbers.splice(index+1,1);
                 ops2.splice(index,1); 
+                numbers;
+                ops2;
+
             }else if(val == '-'){
-                answer += numbers[index]-numbers[index+1];
-                answer;
+                numbers[index]-=numbers[index+1];
+                numbers.splice(index+1,1);
                 ops2.splice(index,1); 
+                numbers;
+                ops2;
             };
 
         })
+        numbers
+        if(numbers.length == 1) answer = numbers[0];
         
-    }
+    };
+    return answer;
+
 }
 
 getAnswer(numbers,ops2);
 console.log('.......ANSWER.......');
-console.log(answer);
+console.log(getAnswer(numbers,ops2));
 console.log('.......ANSWER.......');
