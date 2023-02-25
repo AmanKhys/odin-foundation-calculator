3ae7c0dccb818adb968e1f4c9e6f04b647e015f2
AmanKhys <amankhys2112@gmail.com>
2023-02-26 02:12:12 +0530

    correct getAnswer function logic to solve the math expression in correct precedene

--- script.js
+++ script.js
@@ -77,52 +77,42 @@ return {numbersOkkeAane,ops2Aane};
 //function for evaluating num and ops array into valid math expression
 
 function getAnswer(numbers,ops2){
-    let answer = 0;
-    let len = numbers.length;
-    for(let i = 0;i<len;i++){
-        ops2.forEach((val,index) => {
-            if(val == '/'){
-                if(numbers[index+1] == 0) return '.....math error .....';
-                numbers[index] /=numbers[index+1]; 
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1);//=
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
-            }
-            else if(val == '*'){
-                (numbers[index]*=numbers[index+1]);
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1);
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;//=
-            }
-            else if(val == '+' && !ops2.includes('/') && !ops2.includes('*')){
-                numbers[index]+=numbers[index+1];
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1); 
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
 
-            }else if(val == '-' && !ops2.includes('/') && !ops2.includes('*')){
-                numbers[index]-=numbers[index+1];
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1); 
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
-            };
-
-        })
-        numbers
-        if(numbers.length == 1) answer = numbers[0];
-        
-    };
-    return answer;
-}
-
+    // 
+    for(let i = 0; ops2.includes('/') || ops2.includes('*');)
+    {
+      if(ops2[i] == '-' || ops2[i] == '+') i++;
+      if(ops2[i]== '/'){
+        console.log(i)
+        if(numbers[i+1] == 0) 
+          return;
+        numbers[i]/=numbers[i+1];
+        ops2.splice(i,1);numbers.splice(i+1,1);
+        console.log(i);
+        ops2;numbers;
+      }
+      if(ops2[i]=='*'){
+        console.log(i)
+        numbers[i]*=numbers[i+1];
+        ops2.splice(i,1);numbers.splice(i+1,1);
+      }
+    }
+    for(let i = 0; numbers.length>1;){
+      if(ops2[i]=='-'){
+        numbers[i]-=numbers[i+1];
+        ops2.splice(i,1);numbers.splice(i+1,1);
+      }
+      if(ops2[i]=='+'){
+        numbers[i]+=numbers[i+1];
+        ops2.splice(i,1);numbers.splice(i+1,1);
+      }
+    
+    }
+  let answer = numbers[0];
+  return answer;
+  
+  }
+  
 //update screen on event
 let text = ""
 let prev_value = "";
--- updateScreen.js
+++ updateScreen.js
@@ -4,49 +4,46 @@
 //evaluate to answer
 //wrong logic /// 1*2/2+4-5
 ///I think this function // 1*2 + 2/2 + 2+4 + 4-5 //but that too is not what this function is doing
-let numbers = [1,2,2,4,5];
-let ops2 = ['/','*','-','-'];
+let numbers = [1,2,2,4,5,232,54,12,41,55,22];
+let ops2 = ['-','/','*','-','+','/','/','*','-','*'];
 let len = ops2.length;
+
+
 // function getAnswer(numbers,ops2){
 //     let answer = 0;
+//     let len = numbers.length;
 //     for(let i = 0;i<len;i++){
-//         let skipper = false;
 //         ops2.forEach((val,index) => {
 //             if(val == '/'){
 //                 if(numbers[index+1] == 0) return '.....math error .....';
 //                 numbers[index] /=numbers[index+1]; 
 //                 numbers.splice(index+1,1);
-//                 ops2.splice(index,1);//=
+//                 const lookOp = ops2.splice(index,1);//=
+//                 console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
 //                 numbers;
 //                 ops2;
-//                 skipper = true;
 //             }
 //             else if(val == '*'){
 //                 (numbers[index]*=numbers[index+1]);
 //                 numbers.splice(index+1,1);
-//                 ops2.splice(index,1);
+//                 const lookOp = ops2.splice(index,1);
+//                 console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
 //                 numbers;
 //                 ops2;//=
-//                 skipper = true; //
 //             }
-            
-//         });
-
-//         if(skipper == true) 
-//             continue;
-
-//         ops2.forEach((val,index)=>{
-//             if(val == '+'){
+//             else if(val == '+' && !ops2.includes('/') && !ops2.includes('*')){
 //                 numbers[index]+=numbers[index+1];
 //                 numbers.splice(index+1,1);
-//                 ops2.splice(index,1); 
+//                 const lookOp = ops2.splice(index,1); 
+//                 console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
 //                 numbers;
 //                 ops2;
 
-//             }else if(val == '-'){
+//             }else if(val == '-' && !ops2.includes('/') && !ops2.includes('*')){
 //                 numbers[index]-=numbers[index+1];
 //                 numbers.splice(index+1,1);
-//                 ops2.splice(index,1); 
+//                 const lookOp = ops2.splice(index,1); 
+//                 console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
 //                 numbers;
 //                 ops2;
 //             };
@@ -57,54 +54,48 @@ let len = ops2.length;
         
 //     };
 //     return answer;
-
 // }
 
 function getAnswer(numbers,ops2){
-    let answer = 0;
+
+  // 
+  for(let i = 0; ops2.includes('/') || ops2.includes('*');)
+  {
+    if(ops2[i] == '-' || ops2[i] == '+') i++;
+    if(ops2[i]== '/'){
+      console.log(i)
+      if(numbers[i+1] == 0) 
+        return;
+      numbers[i]/=numbers[i+1];
+      ops2.splice(i,1);
+      numbers.splice(i+1,1);
+      console.log(i);
+      ops2;numbers;
+    }
+    if(ops2[i]=='*'){
+      console.log(i)
+      numbers[i]*=numbers[i+1];
+      ops2.splice(i,1);
+      numbers.splice(i+1,1);
+    }
+  }
+  for(let i = 0; numbers.length>1;){
     let len = numbers.length;
-    for(let i = 0;i<len;i++){
-        ops2.forEach((val,index) => {
-            if(val == '/'){
-                if(numbers[index+1] == 0) return '.....math error .....';
-                numbers[index] /=numbers[index+1]; 
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1);//=
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
-            }
-            else if(val == '*'){
-                (numbers[index]*=numbers[index+1]);
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1);
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;//=
-            }
-            else if(val == '+' && !ops2.includes('/') && !ops2.includes('*')){
-                numbers[index]+=numbers[index+1];
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1); 
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
+    if(ops2[i]=='-'){
+      numbers[i]-=numbers[i+1];
+      ops2.splice(i,1);
+      numbers.splice(i+1,1);
+    }
+    if(ops2[i]=='+'){
+      numbers[i]+=numbers[i+1];
+      ops2.splice(i,1);
+      numbers.splice(i+1,1);
+    }
+  
+  }
+console.log(numbers)
+return numbers[0];
 
-            }else if(val == '-' && !ops2.includes('/') && !ops2.includes('*')){
-                numbers[index]-=numbers[index+1];
-                numbers.splice(index+1,1);
-                const lookOp = ops2.splice(index,1); 
-                console.log(`now doing ${lookOp} operation at ${index} and left with ${numbers }`);
-                numbers;
-                ops2;
-            };
-
-        })
-        numbers
-        if(numbers.length == 1) answer = numbers[0];
-        
-    };
-    return answer;
 }
 
 
@@ -112,7 +103,6 @@ function getAnswer(numbers,ops2){
 
 
 
-getAnswer(numbers,ops2);
 console.log('.......ANSWER.......');
 console.log(getAnswer(numbers,ops2));
 console.log('.......ANSWER.......');
